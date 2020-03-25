1b24f94a870ea7f4709bf6c8fe06ae0cf9c2552a
BeepBoopError <emil0131@gmail.com>
2020-03-25 14:59:57 +0100

    Hade glömt att githubba tills nu
    idag fick jag en hover funktion att funka, så man ser vilka myror som kommer bliselectade

---
+++ Slutprojectet/Assets/Scripts/AntBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntBase : MonoBehaviour
+{
+    public Sprite idle;
+    public Sprite hover;
+    public Sprite selected;
+    bool isHover;
+    public bool isSelect;
+    Queue<Action> actions;
+    SpriteRenderer spriteRender;
+    public GameObject selector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(selector.transform.position.x - (selector.transform.localScale.x/2f) < GetComponent<Transform>().transform.position.x && selector.transform.position.x + (selector.transform.localScale.x/2f) > GetComponent<Transform>().transform.position.x && selector.transform.position.y - (selector.transform.localScale.y/2f) < GetComponent<Transform>().transform.position.y && selector.transform.position.y + (selector.transform.localScale.y/2f) > GetComponent<Transform>().transform.position.y)
+        {
+
+            isHover = true;
+            
+        }
+        else
+        {
+            isHover = false;
+        }
+
+
+        if (isHover)
+        {
+            spriteRender.sprite = hover;
+        }
+        else if (isSelect)
+        {
+            spriteRender.sprite = selected;
+        }
+        else
+        {
+            spriteRender.sprite = idle;
+        }
+    }
+}
---
+++ Slutprojectet/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    private Vector3 startOfSelectMousePos;
+    public List<GameObject> selectedAnts;
+    public GameObject selectorRectangle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectorRectangle.transform.localScale = new Vector3(0f,0f,0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startOfSelectMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //(x1-(x1t-x2t),y1-(y1t-y2t)
+            selectorRectangle.transform.position = new Vector3(startOfSelectMousePos.x - ((startOfSelectMousePos.x * 0.5f) - (currentPos.x * 0.5f)), startOfSelectMousePos.y - ((startOfSelectMousePos.y * 0.5f) - (currentPos.y * 0.5f)), 0f);
+            selectorRectangle.transform.localScale = new Vector3(Mathf.Abs(startOfSelectMousePos.x - currentPos.x),Mathf.Abs(startOfSelectMousePos.y- currentPos.y), 1f);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectorRectangle.transform.localScale = new Vector3(0f,0f,0f);
+        }
+    }
+}
